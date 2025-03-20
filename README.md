1c544c43d4a4b923e3e849d7057e03d7940ce73b
unknown <Th3C0D3B734K37@GMAIL.COM>
2025-03-20 13:34:51 +0530

    Initial commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# parkease-new
---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['localhost'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  // Enable static exports for better offline support
+  output: 'standalone',
+};
+
+export default nextConfig;
+
